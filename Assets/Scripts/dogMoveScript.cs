c9bd2d348eca40fcb03cc83d89ffceb79db71625
ginger-liam <63394901+Ginger-liam@users.noreply.github.com>
2020-09-29 14:35:54 +0200

    dogMoveScriptProgression

--- Assets/Scripts/dogMoveScript.cs
+++ Assets/Scripts/dogMoveScript.cs
@@ -8,14 +8,17 @@ public class dogMoveScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector2(0, -240);
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-        
+        transform.position += new Vector3(0, 1 * Time.deltaTime);
+    }
 
+    void moveUp()
+    {
+      //  transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
     }
 }
