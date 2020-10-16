62fb4674d8d0e8c6907c55c526d0024eab4d00a0
Lanko <youssdgrebb@gmail.com>
2020-10-16 12:01:31 +0200

    Save system v2
    somehow didn't pushed Input and Bullet

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class BulletUpdateScript : MonoBehaviour
+public class BulletScript : MonoBehaviour
 {
-    private int bullets;
+    public int bullets;
     private int incrementScore = 100;
     private int levelScore = 0;
     public Text bulletText;
-    private ScoreScript score;
+    private ScoreScript scoreScript;
+    private InputScript inputScript;
 
     void Awake()
     {
-        score = GameObject.FindObjectOfType<ScoreScript>();
+        scoreScript = FindObjectOfType<ScoreScript>();
+        inputScript = FindObjectOfType<InputScript>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         bullets = 3;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(bullets != 0)
@@ -36,7 +36,7 @@ public class BulletUpdateScript : MonoBehaviour
             if (Input.GetMouseButtonDown(1))
             {
                 levelScore += incrementScore;
-                score.ScoreUpdate(incrementScore);
+                scoreScript.ScoreUpdate(incrementScore);
 
                 if (levelScore % 500 == 0)
                 {
@@ -50,6 +50,7 @@ public class BulletUpdateScript : MonoBehaviour
         else if(bullets >= 0)
         {
             bulletText.text = string.Format("Game over!");
+            inputScript.Show();
         }
     }
 }
---
+++ Assets/Scripts/InputScript.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputScript : MonoBehaviour
+{
+    public Button submitButton;
+    public InputField inputField;
+    public string playerName;
+    private BulletScript bulletScript;
+    private ScoreScript scoreScript;
+
+    void Awake()
+    {
+        scoreScript = FindObjectOfType<ScoreScript>();
+        bulletScript = FindObjectOfType<BulletScript>();
+
+        submitButton.onClick.AddListener(OnClick);
+
+        Hide();
+    }
+
+    void OnClick()
+    {
+        playerName = inputField.text;
+        bulletScript.bullets = 3;
+       
+        ScoreScript.SaveScore saveScore = new ScoreScript.SaveScore
+        {
+            score = scoreScript.currentScore,
+            playerName = playerName
+        };
+
+        string json = JsonUtility.ToJson(saveScore);
+
+        SaveScript.Save(json);
+
+        Debug.Log(string.Format("Score: {0} | Naam: {1}",
+                                scoreScript.currentScore, playerName));
+
+        scoreScript.currentScore = 0;
+        inputField.text = "";
+
+        Hide();
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+}
