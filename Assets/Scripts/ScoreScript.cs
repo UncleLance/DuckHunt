29e9492e95c03ce1c52b9b1e9c48755b097a9758
Ginger-liam <liam-willis@hotmail.com>
2020-10-27 10:14:53 +0100

    ?

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text scoreText;
+    public int currentScore;
+    private InputScript inputScript;
+
+    void Awake()
+    {
+        inputScript = FindObjectOfType<InputScript>();
+    }
+
+    void Update()
+    {
+        scoreText.text = string.Format("Score: {0}", currentScore);
+    }
+    
+    public void ScoreUpdate(int score)
+    {
+        currentScore += score;
+    }
+
+    public class SaveScore
+    {
+        public int score;
+        public string playerName;
+    }
+}
