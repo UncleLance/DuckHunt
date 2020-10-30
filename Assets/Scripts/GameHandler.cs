7dae2a2e3a738461746639312705a797fe44e2f0
Lanko <youssdgrebb@gmail.com>
2020-10-30 10:05:52 +0100

    Save&Load

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour
+{
+    public Button button;
+    public Text text;
+    private List<string> playersList = new List<string>();
+    private List<int> scoresList = new List<int>();
+
+    void Awake()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        SaveScript.Load();
+        FileInfo[] savedData = SaveScript.saveFiles;
+
+        foreach (FileInfo playerData in savedData)
+        {
+            string data = File.ReadAllText(playerData.FullName);
+            ScoreScript.SaveScore loadedData = JsonUtility.FromJson<ScoreScript.SaveScore>(data);
+
+            scoresList.OrderBy(player => loadedData.score).ToList();
+            playersList.Add(loadedData.playerName);
+            /*
+            scoresList.Add(loadedData.score);
+            playersList.Add(loadedData.playerName);
+            */
+        }
+
+        if(playersList.Count > 0)
+        {
+            foreach (string playerName in playersList)
+            {
+                Debug.Log(playerName);
+            }
+        }
+        else
+        {
+            Debug.Log("No players found!");
+        }
+
+        if(scoresList.Count > 0)
+        {
+            foreach (int playerScore in scoresList)
+            {
+                Debug.Log(playerScore);
+            }
+
+        }
+        else
+        {
+            Debug.Log("No scores found!");
+        }
+
+        if (text.text == "")
+        {
+            for (int i = 0; i < scoresList.Count && i < playersList.Count; i++)
+            {
+                string data = string.Format("{0}          {1}\n", scoresList[i], playersList[i]);
+                text.text += data;
+            }
+        }
+
+        /*
+        foreach (string score in scoreList)
+        {
+            string loadedScore = string.Format("{0} \n",  score);
+            text.text += loadedScore;
+        }
+        */
+
+    }
+}
---
+++ Assets/Scripts/SaveScript.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveScript
+{
+    public static readonly string saveFolder = Application.dataPath + "/Saves/";
+    public static FileInfo[] saveFiles;
+
+    public static void InitSaveFolder()
+    {
+        if (!Directory.Exists(saveFolder))
+        {
+            Directory.CreateDirectory(saveFolder);
+        }
+    }
+
+    public static void Save(string saveString)
+    {
+        InitSaveFolder();
+
+        int saveNumber = 1;
+        while (File.Exists(saveFolder + "saveFile_" + saveNumber + ".json"))
+        {
+            saveNumber++;
+        }
+
+        File.WriteAllText(saveFolder + "saveFile_" + saveNumber + ".json", saveString);
+    }
+
+    public static void Load()
+    {
+        DirectoryInfo directory = new DirectoryInfo(saveFolder);
+        saveFiles = directory.GetFiles("*.json");
+
+        /*
+        foreach(FileInfo saveFile in saveFiles)
+        {
+            string save = File.ReadAllText(saveFile.FullName);
+            JsonUtility.FromJson<ScoreScript.SaveScore>(save);
+            Debug.Log(save);
+        }
+        */
+    }
+}
